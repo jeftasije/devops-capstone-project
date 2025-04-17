60c434fa977d5451e4958f1d0a4690a246464a4f
Stefan <nikolovski.stefan14@gmail.com>
2025-04-17 12:57:14 -0400

    implement functionality to update an account

--- service/routes.py
+++ service/routes.py
@@ -91,7 +91,23 @@ def read_account(id):
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
 
-# ... place you code here to UPDATE an account ...
+@app.route("/accounts/<int:id>", methods=["PUT"])
+def update_account(id):
+    """
+    Update an account
+    Update the details of an existing account by its ID.
+
+    This endpoint accepts a PUT request with updated account data
+    and modifies the corresponding account in the database.
+    """
+    account = Account.find(id)
+    if not account:
+        abort(status.HTTP_404_NOT_FOUND, f"Account with  id: [{id}] could not be found.")
+
+    account.deserialize(request.get_json())
+    account.update()
+
+    return account.serialize(), status.HTTP_200_OK
 
 
 ######################################################################
