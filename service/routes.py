4a0ffe9db1c1b40d4131b7c85e40e779ea1f753a
Stefan <nikolovski.stefan14@gmail.com>
2025-04-17 12:00:07 -0400

    implement functionality to retrieve a single account by id

--- service/routes.py
+++ service/routes.py
@@ -68,8 +68,18 @@ def create_accounts():
 # READ AN ACCOUNT
 ######################################################################
 
-# ... place you code here to READ an account ...
+@app.route("/accounts/<int:id>", methods=["GET"])
+def read_account(id):
+    """
+    Reads an account
+    This endpoint will read and dccount based the id provided
+    """
 
+    account = Account.find(id)
+    if not account:
+        abort(status.HTTP_404_NOT_FOUND, f"Account with  id: [{id}] could not be found.")
+
+    return  account.serialize(), status.HTTP_200_OK
 
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -123,4 +123,18 @@ class TestAccountService(TestCase):
         )
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
-    # ADD YOUR TEST CASES HERE ...
+    def test_read_an_account(self):
+        """It should read an account with a specific ID."""
+        account = self._create_accounts(1)[0]
+        response = self.client.get(
+            f"{BASE_URL}/{account.id}",
+            content_type="application/json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.get_json()
+        self.assertEqual(data["name"], account.name)
+
+    def test_account_not_found(self):
+        """It should return not found when reading a non-existent account by ID."""
+        response = self.client.get(f"{BASE_URL}/0",)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
